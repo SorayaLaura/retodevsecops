011cb2729539e86be24e6ee266f6047a6888f9cd
SorayaLaura <31800341+SorayaLaura@users.noreply.github.com>
2025-06-22 20:14:48 -0400

    Actualizacion descripcion

--- Node.js
+++ Node.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('¡Hola, Mundo!'));
+app.get('/', (req, res) => res.send('¡Hola, Laura!'));
 app.listen(3000, () => console.log('Servidor ejecutándose en el puerto 3000'));
